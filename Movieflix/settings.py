ca66fbf4a1960ad75a21d27f027f846b057121d5
BestOlumese <bestolumese@gmail.com>
2024-02-12 10:25:37 +0100

    final

--- Movieflix/settings.py
+++ Movieflix/settings.py
@@ -138,7 +138,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT=os.path.join(BASE_DIR,'assests') 
+STATIC_ROOT=os.path.join(BASE_DIR,'assets') 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFiilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
