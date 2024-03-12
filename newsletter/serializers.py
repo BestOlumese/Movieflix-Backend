10c24ec4b2d6265d329b4eeafe6ebf46e2a65597
BestOlumese <bestolumese@gmail.com>
2024-03-12 15:44:50 +0100

    patch

--- newsletter/serializers.py
+++ newsletter/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Newsletter
 
 class NewsletterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(max_length=500)
-    def create(self, validated_data):
-        return Newsletter.objects.create(**validated_data)
+    class Meta:
+        model = Newsletter
+        fields = ["email"]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,16 +1,16 @@
 from rest_framework.response import Response
 from .models import Newsletter
-from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import NewsletterSerializer 
 from rest_framework import status
 
 # Create your views here.
 @csrf_exempt
-class ApiNewsletterPost(CreateAPIView):
-    def post(self, request, format=None):
-        serializer = NewsletterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ApiNewsletterPost(APIView):
+    def post(self, request, *args, **kwargs):
+        email = request.data
+        newsletter = Newsletter.objects.create(email["email"])
+        newsletter.save()
+        serializer = NewsletterSerializer(newsletter)
+        return Response(serializer.data)
