e632634c64e747cc20a1ee2c45144f42004e72d0
BestOlumese <bestolumese@gmail.com>
2024-03-17 14:31:45 +0100

    patch

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ApiNewsletterPost),
+    path('', views.ApiNewsletterPost.as_view()),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,17 +1,10 @@
-from rest_framework.response import Response
 from .models import Newsletter
-from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import NewsletterSerializer 
-from rest_framework import status
+from rest_framework import generics
 
 # Create your views here.
 @csrf_exempt
-class ApiNewsletterPost(APIView):
-    
-    def post(self, request, *args, **kwargs):
-        email = request.data
-        newsletter = Newsletter.objects.create(email["email"])
-        newsletter.save()
-
-        return 
+class ApiNewsletterPost(generics.CreateAPIView):
+    queryset = Newsletter.objects.all()
+    serializer_class = NewsletterSerializer
