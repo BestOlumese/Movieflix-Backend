415f5e85f8aea9c3787e17b355a6a68ceaea505b
BestOlumese <bestolumese@gmail.com>
2024-03-17 15:27:41 +0100

    patch

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,10 +1,8 @@
 from .models import Newsletter
-from django.views.decorators.csrf import csrf_exempt
 from .serializers import NewsletterSerializer 
 from rest_framework import generics
 
 # Create your views here.
-@csrf_exempt
 class ApiNewsletterPost(generics.ListCreateAPIView):
     queryset = Newsletter.objects.all()
     serializer_class = NewsletterSerializer
